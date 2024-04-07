ddabde08ea40c00c28ab18d747df6e95b2daa1dd
David Palacios <davidpal3c@gmail.com>
2024-04-07 14:48:52 -0600

    test output 1 and 2

--- book.py
+++ book.py
@@ -1,14 +1,15 @@
 
 
+# from test_book import book_list
 
 class Book:
 
-    def __init__(self, isbn, title, author, genre, bool):
+    def __init__(self, isbn, title, author, genre, availability):
         self.__isbn = isbn
         self.__title = title
         self.__author = author
         self.__genre = int(genre)
-        self.__availablitiy = bool
+        self.__availablitiy = bool(availability)
 
 
     def get_isbn(self):
@@ -24,8 +25,13 @@ class Book:
         return self.__genre
     
     def get_availability(self):
+        # if availability == True:
+        #     self.__availablitiy = "Available"
+        #     return self.__availablitiy
+        # else:
+        #     self.__availablitiy = "Borrowed"
+        #     return self.__availablitiy       
         return self.__availablitiy
-
 
     def set_isbn(self, isbn):
         self.__isbn = isbn
@@ -47,7 +53,5 @@ class Book:
         pass
 
 
-
-
     def __str__(self):
-        return f"{self.get_isbn()}"
+        return f"{self.get_isbn()} {self.get_title()} {self.get_author()} {self.get_genre_name()}"  # {self.get_availability(book[4])
