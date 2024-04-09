52c429910c40351e4e59d676cb8c6054a390ddb3
David Palacios <davidpal3c@gmail.com>
2024-04-09 16:02:51 -0600

    get_genre_name ready

--- book.py
+++ book.py
@@ -4,11 +4,22 @@
 
 class Book:
 
-    def __init__(self, isbn, title, author, genre, availability):
+    GENRE = { 0: "Romance",
+                1: "Mystery",
+                2: "Science Fiction",
+                3: "Thriller",
+                4: "Young Adult",
+                5: "Children's Fiction",
+                6: "Self-help",
+                7: "Fantasy",
+                8: "Historical Fiction",
+                9: "Poetry"}
+
+    def __init__(self, isbn, title, author, genre_id, availability):
         self.__isbn = isbn
         self.__title = title
         self.__author = author
-        self.__genre = genre
+        self.__genre = genre_id
         self.__availability = bool(availability)
 
 
@@ -22,8 +33,11 @@ class Book:
         return self.__author
     
     def get_genre_name(self):
-        return self.__genre
+        for key, val in Book.GENRE.items():
+            if key == self.__genre:
+                return val
     
+
     def get_availability(self):
         return "Available" if self.__availability else "Borrowed"
 
@@ -36,8 +50,8 @@ class Book:
     def set_author(self, author):
         self.__author = author
 
-    def set_genre(self, genre):
-        self.__genre = int(genre)
+    def set_genre(self, genre_id):
+        self.__genre = genre_id
   
 
     def borrow_it(self):
@@ -48,4 +62,14 @@ class Book:
 
 
     def __str__(self):
-        return "{:15s} {:26s} {:29s} {:<19} {:<20}".format(self.get_isbn(), self.get_title(), self.get_author(), self.get_genre_name(), self.get_availability())  
+        return "{:15s} {:26s} {:29s} {:<19} {:<20}".format(
+            self.get_isbn(),
+            self.get_title(),
+            self.get_author(),
+            self.get_genre_name(),
+            self.get_availability()
+        )  
+    
+
+genre1 = Book("978-0060513030", "Where the Sidewalk Ends", "Shel Silverstein", 9, False)
+print(genre1.get_genre_name())
